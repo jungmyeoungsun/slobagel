9a813af0f0dce6856abf0d707f959fc0760d71f0
jungmyeoungsun <audtjs2891@naver.com>
2025-08-12 10:09:27 +0900

    슬로베이글

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/slobagel'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
